6bd3321afdf0e79acad879f9401ef4a2501cb461
macadames <macadames@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-09-09 08:07:44 +0000

    tests in progress

--- collective/quickupload/tests/base.py
+++ collective/quickupload/tests/base.py
@@ -1,78 +1,74 @@
-"""Test setup for integration and functional tests.
+# -*- coding: utf-8 -*-
 
-When we import PloneTestCase and then call setupPloneSite(), all of
-Plone's products are loaded, and a Plone site will be created. This
-happens at module level, which makes it faster to run each test, but
-slows down test runner startup.
+"""Base class for quickupload test cases.
 """
 
+import sys
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.Five.testbrowser import Browser
+from Products.CMFCore.utils import getToolByName
+from Products.PloneTestCase.layer import PloneSite
+from Testing import ZopeTestCase as ztc
 from Products.Five import zcml
 from Products.Five import fiveconfigure
-
-from Testing import ZopeTestCase as ztc
-
-from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import onsetup
-
-# When ZopeTestCase configures Zope, it will *not* auto-load products
-# in Products/. Instead, we have to use a statement such as:
-#   ztc.installProduct('SimpleAttachment')
-# This does *not* apply to products in eggs and Python packages (i.e.
-# not in the Products.*) namespace. For that, see below.
-# All of Plone's products are already set up by PloneTestCase.
 
 @onsetup
 def setup_product():
-    """Set up the package and its dependencies.
-
-    The @onsetup decorator causes the execution of this body to be
-    deferred until the setup of the Plone site testing layer. We could
-    have created our own layer, but this is the easiest way for Plone
-    integration tests.
     """
-
-    # Load the ZCML configuration for the example.tests package.
-    # This can of course use <include /> to include other packages.
-
+    Set up the package and its dependencies.
+    """    
+    
     fiveconfigure.debug_mode = True
     import collective.quickupload
-    zcml.load_config('configure.zcml', collective.quickupload)
-    fiveconfigure.debug_mode = False
-
-    # We need to tell the testing framework that these products
-    # should be available. This can't happen until after we have loaded
-    # the ZCML. Thus, we do it here. Note the use of installPackage()
-    # instead of installProduct().
-    # This is *only* necessary for packages outside the Products.*
-    # namespace which are also declared as Zope 2 products, using
-    # <five:registerPackage /> in ZCML.
-
-    # We may also need to load dependencies, e.g.:
-    #   ztc.installPackage('borg.localrole')
-
-    ztc.installPackage('collective.quickupload')
-
-# The order here is important: We first call the (deferred) function
-# which installs the products we need for this product. Then, we let
-# PloneTestCase set up this product on installation.
+    zcml.load_config('configure.zcml',collective.quickupload)
+    #fiveconfigure.debug_mode = False    
+    ztc.installPackage('collective.quickupload')   
 
 setup_product()
 ptc.setupPloneSite(products=['collective.quickupload'])
 
-class TestCase(ptc.PloneTestCase):
-    """We use this base class for all the tests in this package. If
-    necessary, we can put common utility or setup code in here. This
-    applies to unit test cases.
-    """
 
-class FunctionalTestCase(ptc.FunctionalTestCase):
-    """We use this class for functional integration tests that use
-    doctest syntax. Again, we can put basic common utility or setup
-    code in here.
-    """
-
+class QuickUploadTestCase(ptc.FunctionalTestCase):
+    """base test case with convenience methods for all tests""" 
+    
     def afterSetUp(self):
-        roles = ('Member', 'Contributor')
-        self.portal.portal_membership.addMember('contributor',
-                                                'secret',
-                                                roles, [])
+        super(QuickUploadTestCase, self).afterSetUp()        
+        self.browser = Browser()        
+        self.uf = self.portal.acl_users
+        self.uf.userFolderAddUser('root', 'secret', ['Manager'], [])
+        self.uf.userFolderAddUser('contributor', 'secret', ['Member', 'Contributor'], [])        
+        self.ptool = getToolByName(self.portal, 'portal_properties')    
+    
+    def loginAs(self, user, pwd):
+        """points the browser to the login screen and logs in as specified user."""
+        self.browser.open('http://nohost/plone/')
+        self.browser.getLink('Log in').click()
+        self.browser.getControl('Login Name').value = user
+        self.browser.getControl('Password').value = pwd
+        self.browser.getControl('Log in').click()
+
+    def loginAsManager(self, user='root', pwd='secret'):
+        """points the browser to the login screen and logs in as user root with Manager role."""
+        self.loginAs(user='root', pwd='secret')
+    
+    def loginAsContibutor(self):
+        """points the browser to the login screen and logs in as user contributor with Add Content permission."""
+        self.loginAs(user='contributor', pwd='secret')        
+    
+    class layer(PloneSite):
+        @classmethod
+        def setUp(cls):
+            # doctests don't play nicely with ipython
+            try :
+                iphook = sys.displayhook
+                sys.displayhook = sys.__displayhook__
+            except:
+                pass    
+
+        @classmethod
+        def tearDown(cls):
+            try :
+                 sys.displayhook = iphook
+            except:
+                pass    

b1446f81628e20b57e86b51db0951e93ddce1c39
macadames <macadames@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-09-10 08:26:54 +0000

    i18n in progress, french translations completed

--- collective/quickupload/browser/quickupload_settings.py
+++ collective/quickupload/browser/quickupload_settings.py
@@ -13,32 +13,34 @@ class IQuickUploadControlPanel(Interface):
     """
     fields for quick upload control panel
     """
-    use_flashupload = Bool(title=_(u"Use Flashupload"),
-                           description=_(u"By default, the upload script is a javascript only tool. "
-                                          "You can choose to replace it with a Flash upload based script. "
-                                          "Take care : Flash Upload don't work behind an HTTP Authenticated server "
-                                          "(Apache + Basic authentication, IIS + Windows NT Authentication ...) "),
+    use_flashupload = Bool(title=_(u"title_use_flashupload", default=u"Use Flash Upload"),
+                           description=_(u"description_use_flashupload", 
+                                          default=u"By default, the upload script is a javascript only tool. "
+                                          "Check this option to replace it with a Flash Upload based script. "
+                                          "For modern browsers the javascript tool is more powerful. "
+                                          "Flash Upload is just more user friendly under other browsers (MSIE 7, MSIE 8),  "
+                                          "but has many problems : don't work in https, don't work behind HTTP Authentication ..."),
                            default=False,
                            required=False)    
-    auto_upload = Bool(title=_(u"Automatic upload on select"),
-                                 description=_(u"Check if you want to start the files upload on select, without submit. "
+    auto_upload = Bool(title=_(u"title_auto_upload", default=u"Automatic upload on select"),
+                                 description=_(u"description_auto_upload", default=u"Check if you want to start the files upload on select, without submit the form. "
                                                 "Note that you cannot choose file titles "
-                                                "with this option set to True. "),
+                                                "with this option set to True."),
                                  default=False,
                                  required=False) 
-    fill_titles = Bool(title=_(u"Fill title before upload"),
-                                 description=_(u"If checked, you can fill the files titles "
+    fill_titles = Bool(title=_(u"title_fill_titles", default=u"Fill title before upload"),
+                                 description=_(u"description_fill_titles", default=u"If checked, you can fill the files titles "
                                                 "before upload. Uncheck if you don't need titles."),
                                  default=True,
                                  required=False) 
 
-    size_limit = Int( title=_(u"Size limit"),
-                      description=_(u"Size limit for each file in KB, 0 = no limit"),
+    size_limit = Int( title=_(u"title_size_limit", default=u"Size limit"),
+                      description=_(u"description_size_limit", default=u"Size limit for each file in KB, 0 = no limit"),
                       default=0,
                       required=True)
 
-    sim_upload_limit = Int( title=_(u"Silmutaneous uploads limit"),
-                            description=_(u"Number of silmutaneous files uploaded, over this number uploads are placed in a queue, 0 = no limit"),
+    sim_upload_limit = Int( title=_(u"title_sim_upload_limit", default=u"Silmutaneous uploads limit"),
+                            description=_(u"description_sim_upload_limit", default=u"Number of silmutaneous files uploaded, over this number uploads are placed in a queue, 0 = no limit"),
                             default=2,
                             required=True)
 

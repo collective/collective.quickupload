6e132944846c1fd194507e96233a491b017c028d
redcor <redcor@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-03-03 18:29:22 +0000

    conditional import of IVocabularyFactory to make things plone 4.1 compatible

--- collective/quickupload/portlet/vocabularies.py
+++ collective/quickupload/portlet/vocabularies.py
@@ -2,8 +2,11 @@
 
 """ Vocabularies used by control panel or widget
 """
-
-from zope.app.schema.vocabulary import IVocabularyFactory
+try:
+    from zope.app.schema.vocabulary import IVocabularyFactory
+except ImportError:
+    from zope.schema.interfaces import IVocabularyFactory
+    
 from zope.interface import implements
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.vocabulary import SimpleTerm

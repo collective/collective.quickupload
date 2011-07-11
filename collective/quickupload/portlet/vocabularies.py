4554e9c27999f64a5acb2a7df72e17c70e69ef55
thomasdesvenain <thomasdesvenain@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-07-11 17:57:53 +0000

    Fixed portlet field validation.

--- collective/quickupload/portlet/vocabularies.py
+++ collective/quickupload/portlet/vocabularies.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 
 """ Vocabularies used by control panel or widget
 """
@@ -7,12 +7,7 @@ from zope.interface import implements
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.vocabulary import SimpleTerm
-try:
-    # Zope 2.13
-    from zope.component.hooks import getSite
-except ImportError:
-    # Zope 2.12
-    from zope.site.hooks import getSite
+
 from Products.ATContentTypes.interfaces import IFileContent, IImageContent
 from Products.CMFCore.utils import getToolByName
 
@@ -23,9 +18,9 @@ def _listTypesForInterface(portal, interface):
     @param interface: Zope 2 inteface
     @return: [{'portal_type': xx, 'type_ui_info': UI type info}, ...]
     """
- 
+
     archetype_tool = getToolByName(portal, 'archetype_tool')
-    
+
     #plone4
     try :
         all_types = [tipe.getId() for tipe in archetype_tool.listPortalTypesWithInterfaces([interface])]
@@ -34,19 +29,19 @@ def _listTypesForInterface(portal, interface):
         all_types = archetype_tool.listRegisteredTypes(inProject=True)
         all_types = [tipe['portal_type'] for tipe in all_types
                      if interface.isImplementedByInstancesOf(tipe['klass'])]
-    
+
     # fix for bug in listRegisteredTypes which returns 2 'ATFolder'
     # when asking for IBaseFolder interface
-    unik_types = dict.fromkeys(all_types).keys() 
+    unik_types = dict.fromkeys(all_types).keys()
     return [_infoDictForType(portal, tipe) for tipe in unik_types]
-    
+
 def _infoDictForType(portal, ptype ):
     """
     UI type infos
     @param ptype: a portal type name
     @return: {'portal_type': xxx, 'type_ui_info': UI type info}
     """
- 
+
     portal_types = getToolByName(portal, 'portal_types')
     type_info = getattr(portal_types, ptype)
     title = type_info.Title()
@@ -56,7 +51,7 @@ def _infoDictForType(portal, ptype ):
     return {
         'portal_type': ptype,
         'type_ui_info': type_ui_info
-        }    
+        }
 
 class UploadFileTypeVocabulary(object):
     """Vocabulary factory for file type upload
@@ -65,18 +60,18 @@ class UploadFileTypeVocabulary(object):
 
     def __call__(self, context):
         context = getattr(context, 'context', context)
-        portal = getSite()
+        portal = getToolByName(context, 'portal_url').getPortalObject()
         flt = _listTypesForInterface(portal, IFileContent)
         ilt = _listTypesForInterface(portal, IImageContent)
-        items = [SimpleTerm('auto', 'auto', context.translate('label_default_portaltype_configuration', 
-                                                      default=u'Default configuration (Content Type Registry).', 
+        items = [SimpleTerm('auto', 'auto', context.translate('label_default_portaltype_configuration',
+                                                      default=u'Default configuration (Content Type Registry).',
                                                       domain='collective.quickupload')),]
         items.extend([ SimpleTerm(t['portal_type'], t['portal_type'], t['type_ui_info'])
                   for t in flt ])
         items.extend([ SimpleTerm(t['portal_type'], t['portal_type'], t['type_ui_info'])
                   for t in ilt ])
-        return SimpleVocabulary(items)        
-        
+        return SimpleVocabulary(items)
+
 
 UploadFileTypeVocabularyFactory = UploadFileTypeVocabulary()
 

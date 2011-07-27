669349698c8313a61f7c97b6f3953b183a128fa1
thomasdesvenain <thomasdesvenain@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-07-27 11:50:24 +0000

    css are restricted to authenticated users too

--- collective/quickupload/upgrades/upgrades.py
+++ collective/quickupload/upgrades/upgrades.py
@@ -2,3 +2,5 @@
 def v1_v2(context):
     context.runImportStepFromProfile('profile-collective.quickupload:default',
                                      'jsregistry')
+    context.runImportStepFromProfile('profile-collective.quickupload:default',
+                                     'cssregistry')

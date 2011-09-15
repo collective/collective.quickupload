77f3bc4f6df0133145d6a46ae4d099645794d510
hvelarde <hvelarde@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-09-15 17:32:35 +0000

    added support for tests using plone.app.testing; control panel is now removed when package is uninstalled

---
+++ collective/quickupload/tests/test_controlpanel.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from zope.component import getMultiAdapter
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import logout
+from plone.app.testing import setRoles
+
+from collective.quickupload.testing import INTEGRATION_TESTING
+
+
+class ControlPanelTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.cp = getattr(self.portal, 'portal_controlpanel')
+
+    def test_installed(self):
+        # entry is in the control panel
+        installed = [a.getAction(self)['id'] for a in self.cp.listActions()]
+        self.failUnless('QuickUpload' in installed)
+
+    def test_uninstalled(self):
+        setup_tool = getattr(self.portal, 'portal_setup')
+        setup_tool.runAllImportStepsFromProfile('profile-collective.quickupload:uninstall')
+
+        # entry is removed from the control panel
+        installed = [a.getAction(self)['id'] for a in self.cp.listActions()]
+        self.failUnless('QuickUpload' in installed)
+
+    def test_view(self):
+        view = getMultiAdapter((self.portal, self.portal.REQUEST),
+                               name='quickupload-controlpanel')
+        view = view.__of__(self.portal)
+        self.failUnless(view())
+
+    def test_view_protected(self):
+        # view can not be accessed by anonymous users
+        from AccessControl import Unauthorized
+        logout()
+        self.assertRaises(Unauthorized,
+                          self.portal.restrictedTraverse,
+                          '@@quickupload-controlpanel')
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
--- setup.py
+++ setup.py
@@ -22,11 +22,13 @@ setup(name='collective.quickupload',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
       entry_points="""
-          [z3c.autoinclude.plugin]
-          target = plone
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )

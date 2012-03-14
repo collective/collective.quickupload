176894c47d9b6b6f6322d4916672c593450f3500
tdesvenain <thomas.desvenain@gmail.com>
2012-03-14 19:00:51 +0100

    add upgrade step

--- collective/quickupload/upgrades/upgrades.py
+++ collective/quickupload/upgrades/upgrades.py
@@ -1,6 +1,15 @@
 from Products.CMFCore.utils import getToolByName
 
+
 def v1_v2(context):
+    upgrade_resources(context)
+
+
+def v2_v3(context):
+    upgrade_resources(context)
+
+
+def upgrade_resources(context):
     context.runImportStepFromProfile('profile-collective.quickupload:default',
                                      'jsregistry')
     context.runImportStepFromProfile('profile-collective.quickupload:default',

10f7af8cf325a51ef37612b5ec045ff7af9d523a
Wolfgang Thomas <thomas@syslab.com>
2013-03-21 04:16:52 +0100

    Added a control-panel setting that determines if the Upload action is shown. Additionally, don't show the Upload action if the upload portlet is present in the current context

--- collective/quickupload/browser/utils.py
+++ collective/quickupload/browser/utils.py
@@ -1,15 +1,40 @@
 # -*- coding: utf-8 -*-
 
 from Acquisition import aq_inner
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.Five.browser import BrowserView
-from zope.component import getMultiAdapter
+from collective.quickupload.browser.quickupload_settings import \
+    IQuickUploadControlPanel
+from collective.quickupload.portlet.quickuploadportlet import \
+    IQuickUploadPortlet
+from plone.app.portlets.utils import assignment_mapping_from_key
+from plone.portlets.constants import CONTEXT_CATEGORY
+from plone.portlets.interfaces import IPortletManager
+from zope.component import getMultiAdapter, getUtility, getUtilitiesFor
 
 
 class QuickuploadHelper(BrowserView):
 
     def show_quickupload_action(self):
+        portal = getUtility(IPloneSiteRoot)
+        qup_prefs = IQuickUploadControlPanel(portal)
+        if not qup_prefs.show_upload_action:
+            return False
         context_state = getMultiAdapter(
             (aq_inner(self.context), self.request),
             name=u'plone_context_state')
-        show = context_state.is_default_page() or context_state.is_folderish()
-        return show
+        # If the Quickuploader portlet is shown in the current context, don't
+        # show the Upload action, since the portlet takes precedence
+        portlet_manager_names = [
+            x[0] for x in getUtilitiesFor(IPortletManager)
+            if not x[0].startswith('plone.dashboard')
+        ]
+        path = '/'.join(self.context.getPhysicalPath())
+        for name in portlet_manager_names:
+            portlets = assignment_mapping_from_key(
+                self.context, name, CONTEXT_CATEGORY, path).values()
+            for portlet in portlets:
+                if IQuickUploadPortlet.providedBy(portlet):
+                    return False
+
+        return context_state.is_default_page() or context_state.is_folderish()

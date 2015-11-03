6c9d9e031436ef2fd3ee305ada482d3a66f35fe6
Maurits van Rees <maurits@vanrees.org>
2015-11-03 18:07:16 +0100

    Made update.sh i18n script a bit prettier.
    The backslashes were not needed.
    And forced removal of rebuild_i18n.log, otherwise you get an error message when it does not exist.

--- collective/quickupload/locales/update.sh
+++ collective/quickupload/locales/update.sh
@@ -2,10 +2,10 @@ domain='collective.quickupload'
 
 # Synchronise the templates and scripts with the .pot file for collective.quickupload domain.
 i18ndude rebuild-pot --pot $domain.pot --merge ./$domain-manual.pot --create $domain ../
-i18ndude rebuild-pot --pot plone.pot --merge ./plone-manual.pot 
+i18ndude rebuild-pot --pot plone.pot --merge ./plone-manual.pot
 
 # Synchronise the collective.quickupload's pot file (Used for the workflows)
-i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po 
+i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
 i18ndude sync --pot plone.pot */LC_MESSAGES/plone.po
 
 WARNINGS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
@@ -13,13 +13,13 @@ ERRORS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR
 FATAL=`find . -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
 
 echo
-echo "There are $WARNINGS warnings \(possibly missing i18n markup\)"
-echo "There are $ERRORS errors \(almost definitely missing i18n markup\)"
-echo "There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)"
-echo "For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or"
-echo "Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir"
+echo "There are $WARNINGS warnings (possibly missing i18n markup)"
+echo "There are $ERRORS errors (almost definitely missing i18n markup)"
+echo "There are $FATAL fatal errors (template could not be parsed, eg. if it's not html)"
+echo "For more details, run 'find . -name "*pt" | xargs i18ndude find-untranslated' or"
+echo "Look the rebuild i18n log generate for this script called 'rebuild_i18n.log' on locales dir"
 
-rm ./rebuild_i18n.log
+rm -f ./rebuild_i18n.log
 
 touch ./rebuild_i18n.log
 

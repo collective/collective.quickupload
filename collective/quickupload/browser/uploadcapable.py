ace48e07ce5046a0a564ce4197b1939902919273
Maurits van Rees <maurits@vanrees.org>
2015-11-12 23:27:47 +0100

    browser/uploadcapable.py: normal line endings

--- collective/quickupload/browser/uploadcapable.py
+++ collective/quickupload/browser/uploadcapable.py
@@ -62,7 +62,7 @@ def get_id_from_filename(filename, context, unique=False):
     return newid
 
 
-class QuickUploadCapableFileFactory(object):
+class (object):
     interface.implements(IQuickUploadFileFactory)
     component.adapts(IQuickUploadCapable)
 

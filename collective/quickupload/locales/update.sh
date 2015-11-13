677e12868d7220a3b06b3bf0205d97b812661716
Maurits van Rees <maurits@vanrees.org>
2015-11-13 14:23:06 +0100

    Simplified i18ndude find-untranslated call.
    There is no need for 'find' and 'xargs': just point i18ndude to a directory.

--- collective/quickupload/locales/update.sh
+++ collective/quickupload/locales/update.sh
@@ -8,19 +8,19 @@ i18ndude rebuild-pot --pot plone.pot --merge ./plone-manual.pot
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
 i18ndude sync --pot plone.pot */LC_MESSAGES/plone.po
 
-WARNINGS=`find .. -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
-ERRORS=`find .. -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
-FATAL=`find .. -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
+WARNINGS=`i18ndude find-untranslated .. | grep -e '^-WARN' | wc -l`
+ERRORS=`i18ndude find-untranslated .. | grep -e '^-ERROR' | wc -l`
+FATAL=`i18ndude find-untranslated .. | grep -e '^-FATAL' | wc -l`
 
 echo
 echo "There are $WARNINGS warnings (possibly missing i18n markup)"
 echo "There are $ERRORS errors (almost definitely missing i18n markup)"
 echo "There are $FATAL fatal errors (template could not be parsed, eg. if it's not html)"
-echo "For more details, run 'find . -name "*pt" | xargs i18ndude find-untranslated' or"
+echo "For more details, run 'i18ndude find-untranslated ..' or"
 echo "Look the rebuild i18n log generate for this script called 'rebuild_i18n.log' on locales dir"
 
 rm -f ./rebuild_i18n.log
 
 touch ./rebuild_i18n.log
 
-find .. -name "*pt" | xargs i18ndude find-untranslated > ./rebuild_i18n.log
+i18ndude find-untranslated .. > ./rebuild_i18n.log
